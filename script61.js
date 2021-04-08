57a3a574e78ddd9b0a84ef9f47a3b05693eafa6e
Nataha-ha <nataly.matveeva95@gmail.com>
2021-04-08 21:31:29 +0300

    script61 change

--- script61.js
+++ script61.js
@@ -1,44 +1,65 @@
-var imageSmallBoat = document.querySelector('.image-small-boat');
-var imageBigBoat = document.querySelector('.image-big-boat');
-
-function showImageBigBoat() {
-    imageBigBoat.style.display = 'block';
-    // imageSmallBoat.removeEventListener('click', showImageBigBoat);
+window.onload = function(){  //при загрузке всех тэгов страницы запускается функция
+    var image = document.getElementsByTagName("img");//массив картинок
+    for(var i=0;i<image.length;i++){
+        image[i].onclick=showBigImage;
+    }
 }
 
-imageSmallBoat.addEventListener('click', showImageBigBoat);
 
 
-
-var imageSmallDawn = document.querySelector('.image-small-dawn');
-var imageBig = document.querySelector('.image-big');
-var imageBigDawn = document.createElement('img');
-
-function showImageBigDawn() {
-    imageBigDawn.src = 'img/dawn_1280.jpg';
-    imageBigDawn.className = 'image-big-boat';
-    imageBig.append(imageBigDawn);
-    imageBigDawn.style.display = 'block';
-
-
-}
-imageSmallDawn.addEventListener('click', showImageBigDawn);
-
-
-var imageSmallTree = document.querySelector('.image-small-tree');
-
-function showImageBigTree() {
-    alert('Изображение не найдено');
+function showBigImage(event) {
+    var bigImageBlock = document.querySelector('.image-big');
+    bigImageBlock.innerHTML = "";
+    var smallImage = event.target;
+    var imageNameParts = smallImage.id.split("_");
+    var path = 'imgBig/'+imageNameParts[1]+'.jpg';
+	var bigImage = document.createElement('img');
+	bigImage.src = path;
+	bigImageBlock.appendChild(bigImage);
+	bigImage.onerror = function(){
+        //bigImageBlock.removeChild(bigImage);
+        alert("file not found");		
+		}
 }
 
-imageSmallTree.addEventListener('click', showImageBigTree);
 
+
+
+// imageSmallBoat.addEventListener('click', showImageBigBoat);
+
+
+
+
+// var imageSmallDawn = document.querySelector('.image-small-dawn');
+// var imageBig = document.querySelector('.image-big');
+// var imageBigDawn = document.createElement('img');
+
+// function showImageBigDawn() {
+//     imageBigDawn.src = 'img/dawn_1280.jpg';
+//     imageBigDawn.className = 'image-big-boat';
+//     imageBig.append(imageBigDawn);
+//     imageBigDawn.style.display = 'block';
+
+
+// }
+// imageSmallDawn.addEventListener('click', showImageBigDawn);
+
+
+// var imageSmallTree = document.querySelector('.image-small-tree');
+
+// function showImageBigTree() {
+//     alert('Изображение не найдено');
+// }
+
+// imageSmallTree.addEventListener('click', showImageBigTree);
+
+// imageSmallBoat.hide();
 
 // function hide() {
-//     if (imageBigBoat.style.display == 'none') {
-//         imageBigBoat.style.display = 'block';
+//     if (imageBig.style.display == 'none') {
+//         imageBig.style.display = 'block';
 //     } else {
-//         imageBigBoat.style.display = 'none';
+//         imageBig.style.display = 'none';
 //     }
 //     imageSmallBoat.removeEventListener('click', hide);
 // }
